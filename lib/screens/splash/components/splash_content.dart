71b3b9a3a9a139fb5584decaf36a56b0dee69e4c
Putraaa <61768347+vickycxc@users.noreply.github.com>
2021-11-16 00:05:14 +0700

    login screen background

--- lib/screens/splash/components/splash_content.dart
+++ lib/screens/splash/components/splash_content.dart
@@ -12,50 +12,55 @@ class SplashContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset(splashData['image']!,fit: BoxFit.cover,
-          height: SizeConfig.screenHeight,
-          width: SizeConfig.screenWidth,),
-        Column(
+    return Container(
+      color: Colors.black,
+      child: SafeArea(
+        child: Stack(
           children: [
-            Expanded(child: Container()),
-            Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [
-                        Colors.black,
-                        Colors.transparent
-                      ],
-                      stops: [0.2, 1]
-                  )
-              ),
-              width: SizeConfig.screenWidth,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.02, vertical: SizeConfig.screenHeight * 0.02),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(alignment: AlignmentDirectional.bottomStart,child: Text(splashData['title']!,style: TextStyle(fontSize: 40,color: Colors.white, fontWeight: FontWeight.w700,fontFamily: 'League Spartan'),)),
-                    // SizedBox(height: SizeConfig.screenHeight * 0.1)
-                    SizedBox(height: SizeConfig.screenHeight * 0.01,),
-                    Row(
+            Image.asset(splashData['image']!,fit: BoxFit.cover,
+              height: SizeConfig.screenHeight,
+              width: SizeConfig.screenWidth,),
+            Column(
+              children: [
+                Expanded(child: Container()),
+                Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Colors.black,
+                            Colors.transparent
+                          ],
+                          stops: [0.2, 1]
+                      )
+                  ),
+                  width: SizeConfig.screenWidth,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.03, vertical: SizeConfig.screenHeight * 0.02),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(child: Text(splashData['subtitle']!, style: TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.w300,fontFamily: 'Nexa'),),flex: 4,),
-                        Expanded(child: Container(),)
+                        Align(alignment: AlignmentDirectional.bottomStart,child: Text(splashData['title']!,style: TextStyle(fontSize: 40,color: Colors.white, fontWeight: FontWeight.w700,fontFamily: 'League Spartan'),)),
+                        // SizedBox(height: SizeConfig.screenHeight * 0.1)
+                        SizedBox(height: SizeConfig.screenHeight * 0.01,),
+                        Row(
+                          children: [
+                            Expanded(child: Text(splashData['subtitle']!, style: TextStyle(fontSize: 12,color: Colors.white, fontWeight: FontWeight.w300,fontFamily: 'Nexa'),),flex: 4,),
+                            Expanded(child: Container(),)
+                          ],
+                        ),
+                        SizedBox(height: SizeConfig.screenHeight * 0.05,),
+
                       ],
                     ),
-                    SizedBox(height: SizeConfig.screenHeight * 0.05,),
-
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
