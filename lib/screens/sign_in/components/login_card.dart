bc60c1dcd1dd694b4f13a8225515a68951a2cb10
Putraaa <61768347+vickycxc@users.noreply.github.com>
2021-11-17 23:20:39 +0700

    merapikan login textform

---
+++ lib/screens/sign_in/components/login_card.dart
@@ -0,0 +1,91 @@
+import 'package:fiksii/components/no_account_text.dart';
+import 'package:fiksii/screens/sign_in/components/sign_form.dart';
+import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+
+import '../../../size_config.dart';
+
+class LoginCard extends StatelessWidget {
+  const LoginCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassmorphicContainer(
+      width: SizeConfig.screenWidth * 0.9,
+      height: SizeConfig.safeScreenHeight * 0.9,
+      borderRadius: 32,
+      border: 4,
+      blur: 2,
+      borderGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFffffff).withOpacity(0.0),
+          Color(0xFFffffff).withOpacity(0.00),
+        ],
+        stops: [0.1, 1],
+      ),
+      linearGradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFffffff).withOpacity(0.1),
+          Color(0xFFffffff).withOpacity(0.05),
+        ],
+        stops: [0.1, 1],
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        // width: SizeConfig.screenWidth * 0.8,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            GlassmorphicContainer(
+              height: getProportionateScreenWidth(SizeConfig.screenWidth * 0.4),
+              width: getProportionateScreenWidth(SizeConfig.screenWidth * 0.4),
+              // padding: EdgeInsets.all(8),
+              borderGradient: LinearGradient(colors: [
+                Colors.white.withOpacity(0.1),
+                Colors.white.withOpacity(0.05),
+              ]),
+              blur: 2,
+              border: 4,
+              borderRadius: 200,
+              linearGradient: LinearGradient(colors: [
+                Colors.white.withOpacity(0.1),
+                Colors.white.withOpacity(0.05),
+              ],
+                  stops: [0.5, 0.9],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Image.asset('assets/logo/ordero_logo_transparent.png'),
+              ),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(20),
+            ),
+            Text('Sign In',style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w800,
+                fontSize: getProportionateScreenWidth(28),
+                color: Colors.white.withOpacity(0.6)),),
+            SizedBox(
+              height: getProportionateScreenHeight(40),
+            ),
+            SignForm(),
+            Spacer(),
+            Row(),
+            NoAccountText(),
+            Spacer(flex: 3,)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/screens/sign_in/components/new_body.dart
+++ lib/screens/sign_in/components/new_body.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 
 import '../../../size_config.dart';
+import 'login_card.dart';
 
 class NewBody extends StatelessWidget {
   const NewBody({Key? key}) : super(key: key);
@@ -52,80 +53,7 @@ class NewBody extends StatelessWidget {
                 child: SingleChildScrollView(
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: SizeConfig.screenWidth * 0.05, vertical: SizeConfig.safeScreenHeight * 0.05),
-                    child: GlassmorphicContainer(
-                      width: SizeConfig.screenWidth * 0.9,
-                      height: SizeConfig.safeScreenHeight * 0.9,
-                      borderRadius: 32,
-                      border: 4,
-                      blur: 2,
-                      borderGradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color(0xFFffffff).withOpacity(0.0),
-                          Color(0xFFffffff).withOpacity(0.00),
-                        ],
-                        stops: [0.1, 1],
-                      ),
-                      linearGradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color(0xFFffffff).withOpacity(0.1),
-                          Color(0xFFffffff).withOpacity(0.05),
-                        ],
-                        stops: [0.1, 1],
-                      ),
-                      child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 18),
-                            // width: SizeConfig.screenWidth * 0.8,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Spacer(),
-                                GlassmorphicContainer(
-                                  height: getProportionateScreenWidth(SizeConfig.screenWidth * 0.4),
-                                  width: getProportionateScreenWidth(SizeConfig.screenWidth * 0.4),
-                                  // padding: EdgeInsets.all(8),
-                                  borderGradient: LinearGradient(colors: [
-                                    Colors.white.withOpacity(0.1),
-                                    Colors.white.withOpacity(0.05),
-                                  ]),
-                                  blur: 2,
-                                  border: 4,
-                                  borderRadius: 200,
-                                  linearGradient: LinearGradient(colors: [
-                                    Colors.white.withOpacity(0.1),
-                                    Colors.white.withOpacity(0.05),
-                                  ],
-                                      stops: [0.5, 0.9],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                    child: Image.asset('assets/logo/ordero_logo_transparent.png'),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: getProportionateScreenHeight(20),
-                                ),
-                                Text('Sign In',style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w800,
-                                    fontSize: getProportionateScreenWidth(28),
-                                    color: Colors.white.withOpacity(0.6)),),
-                                SizedBox(
-                                  height: getProportionateScreenHeight(40),
-                                ),
-                                SignForm(),
-                                Spacer(),
-                                Row(),
-                                NoAccountText(),
-                                Spacer(flex: 3,)
-                              ],
-                            ),
-                          ),
-                    ),
+                    child: LoginCard(),
                   ),
                 ),
               )
@@ -136,4 +64,3 @@ class NewBody extends StatelessWidget {
     );
   }
 }
-
